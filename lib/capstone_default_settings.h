3a92776e76ca896df0b3a230500e0e5c92b71823
Ray Liu <ray@rayliu.ca>
2020-12-07 20:18:48 -0700

    Update capstone_default_settings.h

--- lib/capstone_default_settings.h
+++ lib/capstone_default_settings.h
@@ -1,3 +1,4 @@
+
 #define DEVICE_TYPE 0 // Hub = 0, Seneor = 1, switch = 2, 
 #define POWER_SOURCE 0 // wall power= 0, battery = 1
 #define DEVICE_ID String(random(0xffff), HEX) // use mac address in the future: WiFi.macAddress()
